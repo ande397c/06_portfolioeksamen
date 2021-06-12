b173dfe10ab33bf5a6d24d0a5a6d47a4d88e8543
“ande397c” <ande397c@stud.kea.dk>
2021-06-12 22:31:51 +0200

    indhold til tema sider + links på forside

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ window.addEventListener("load", SidenVises);
 
 const knap = document.querySelector("#burger_knap");
 const links = document.querySelector(".navbar_links");
+const om = document.querySelector("#mig");
 
 function SidenVises() {
   console.log("SidenVises");
   knap.addEventListener("click", toggleMenu);
+  om.addEventListener("click", omSide);
+}
+
+function omSide() {
+  console.log("omSide");
+  window.location.replace("om.html");
 }
 
 function toggleMenu() {
